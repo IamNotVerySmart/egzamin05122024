72ad5bc8eadb84ef2677be840b7f5d41991fed15
IamNotVerySmart <93706080+IamNotVerySmart@users.noreply.github.com>
2024-12-05 13:14:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using egzamin0512;
+List<osoba> osobas = new List<osoba>();
+using (var reader = new StreamReader(@"dane_pracownikow.csv"))
+{
+    while(!reader.EndOfStream)
+    {
+        var data = reader.ReadLine().Split(",");
+        if (data[0] != "Imię" & data[1] != "Nazwisko" & data[2] != "Pensja")
+            osobas.Add(new osoba(data[0], data[1], int.Parse(data[2])));
+    }
+
+    foreach (var osoba in osobas)
+        Console.WriteLine(osoba.ToString());
+}
+
+using (var writer = new StreamWriter(@"filtrowani_pracownicy.csv"))
+{
+    List<osoba> humans = osoba.humans(osobas);
+    foreach(var h in humans)
+    {
+        writer.WriteLine(h.ToString());
+    }
+    Console.WriteLine("AAAAAAAAAA");
+}
---
+++ osoba.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egzamin0512
+{
+    internal class osoba
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Pay {  get; set; }
+
+        public osoba(string name, string surname, int pay)
+        {
+            Name = name;
+            Surname = surname;
+            Pay = pay;
+        }
+
+        public string ToString()
+        {
+            return this.Name + "," + this.Surname + "," + this.Pay;
+        }
+
+        public static List<osoba> humans(List<osoba> osobas)
+        {
+            List<osoba> humans = new List<osoba>();
+            foreach (osoba a in osobas)
+            {
+                if (a.Pay > 5000)
+                {
+                    humans.Add(a);
+                }
+            }
+            return humans;
+        }
+
+
+    }
+}
